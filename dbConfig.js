6221b221c84ad61f2e074f107f95824c1be29096
Manuel-Alexandre <goisalexandre835@gmail.com>
2023-07-22 20:38:22 -0300

    Comentado os outros valores

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: "postgresql://postgres:QbZ5vOPLAlsTDErJ43jr@containers-us-west-82.railway.app:7146/railway",
-  host: "containers-us-west-82.railway.app",
-  user:"postgres",
-  password: "QbZ5vOPLAlsTDErJ43jr",
-  database: "railway",
-  port: "7146"
+  // host: "containers-us-west-82.railway.app",
+  // user:"postgres",
+  // password: "QbZ5vOPLAlsTDErJ43jr",
+  // database: "railway",
+  // port: "7146"
 };
